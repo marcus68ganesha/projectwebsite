96c9e33b0c819a5beba0e22cd0aef3572f469cf2
marcus68ganesha <53260939+marcus68ganesha@users.noreply.github.com>
2022-03-27 21:41:36 -1200

    Add files via upload

---
+++ Javascript Web Learning/demo.js
@@ -0,0 +1,18 @@
+document.write("<h1>Welcome To the Real World of Coding</h1>");
+
+document.write("<h2>Let's Learn Coding Together</h2>");
+
+document.write("<h3>We can change the world</h3>");
+
+
+/*var x; this is for declaration
+   x=5; this is for initialization
+ */
+
+var number = 16; //number
+var Name = "Alex"; //string
+var flag = true; // boolean
+
+document.write("<h1>"+number+"</h1");// printing on browser
+//alert(Name);//for display the command window once you start enter the page
+
---
+++ Javascript Web Learning/operator.js
@@ -0,0 +1,6 @@
+// Operators
+var a = 3;
+var b = 4;
+
+var result = a + b;
+document.write("<h2>the result: "+result+"</h2>");
